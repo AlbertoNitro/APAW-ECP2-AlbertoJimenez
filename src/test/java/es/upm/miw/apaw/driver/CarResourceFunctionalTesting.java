6c0c8d76762f162d36f9ca5a3f55ec51a0c94599
AlbertoNitro <albertojgm@yahoo.es>
2017-10-06 19:01:52 +0200

    Test de funcionalidad POST /cars

--- src/test/java/es/upm/miw/apaw/driver/CarResourceFunctionalTesting.java
+++ src/test/java/es/upm/miw/apaw/driver/CarResourceFunctionalTesting.java
@@ -7,6 +7,44 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-public class CarResourceFunctionalTesting {
+import es.upm.miw.apaw.driver.api.daos.DaoFactory;
+import es.upm.miw.apaw.driver.api.daos.memory.DaoMemoryFactory;
+import es.upm.miw.apaw.driver.api.resources.CarResource;
+import es.upm.miw.apaw.driver.api.resources.DriverResource;
+import es.upm.miw.apaw.driver.http.HttpClientService;
+import es.upm.miw.apaw.driver.http.HttpException;
+import es.upm.miw.apaw.driver.http.HttpMethod;
+import es.upm.miw.apaw.driver.http.HttpRequest;
+import es.upm.miw.apaw.driver.http.HttpRequestBuilder;
+import es.upm.miw.apaw.driver.http.HttpStatus;
 
+public class CarResourceFunctionalTesting {
+    @Before
+    public void before() {
+        DaoFactory.setFactory(new DaoMemoryFactory());
+    }
+
+    private void createCar() {
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(CarResource.CARS).body("1").build();
+        new HttpClientService().httpRequest(request);
+    }
+
+    @Test
+    public void testCreateCar() {
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(CarResource.CARS).body("1").build();
+        assertEquals(HttpStatus.CREATED, new HttpClientService().httpRequest(request).getStatus());
+
+    }
+
+    @Test(expected = HttpException.class)
+    public void testCreateCarIdEmpty() {
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(CarResource.CARS).body("").build();
+        new HttpClientService().httpRequest(request);
+    }
+
+    @Test(expected = HttpException.class)
+    public void testCreateWithoutCarId() {
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(CarResource.CARS).build();
+        new HttpClientService().httpRequest(request);
+    }
 }
