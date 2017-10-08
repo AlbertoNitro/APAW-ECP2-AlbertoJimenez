19567222ebd00e2082f35c14641d1db711ac02f2
AlbertoNitro <albertojgm@yahoo.es>
2017-10-09 00:51:17 +0200

    Comprobar test

--- src/test/java/es/upm/miw/apaw/driver/api/AllDriverApiTests.java
+++ src/test/java/es/upm/miw/apaw/driver/api/AllDriverApiTests.java
@@ -6,13 +6,15 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import es.upm.miw.apaw.driver.api.controllers.AllDriverApiControllersTests;
 import es.upm.miw.apaw.driver.api.daos.memory.AllDriverApiDaosMemoryTests;
+import es.upm.miw.apaw.driver.api.dtos.AllDriverApiDtosTests;
 import es.upm.miw.apaw.driver.api.entities.AllDriverApiEntitiesTests;
 
 @RunWith(Suite.class)
 @SuiteClasses ({
     AllDriverApiControllersTests.class,
     AllDriverApiEntitiesTests.class,
-    AllDriverApiDaosMemoryTests.class
+    AllDriverApiDaosMemoryTests.class,
+    AllDriverApiDtosTests.class
 })
 public class AllDriverApiTests {
 
